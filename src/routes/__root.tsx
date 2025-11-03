b01472589ead210577fc3795b311d7679e9cf5ea
Brandon Harris <brandon@relentnet.com>
2025-11-03 11:24:18 -0600

    Added vercel analytics as well.

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,5 +1,6 @@
 import { Outlet, createRootRoute } from '@tanstack/react-router'
 import { SpeedInsights } from '@vercel/speed-insights/react'
+import { Analytics } from '@vercel/analytics/next'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 import { AuthProvider } from '../components/AuthProvider'
@@ -8,6 +9,7 @@ export const Route = createRootRoute({
   component: () => (
     <AuthProvider>
       <SpeedInsights />
+      <Analytics />
       <Header />
       <Outlet />
       <Footer />
