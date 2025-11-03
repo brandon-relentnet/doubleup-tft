8125e0ca779b62197292e3ab0e3c5562679e7815
Brandon Harris <brandon@relentnet.com>
2025-11-03 11:29:16 -0600

    Accidentally added the analytics for nextjs rather than react.

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,6 +1,6 @@
 import { Outlet, createRootRoute } from '@tanstack/react-router'
 import { SpeedInsights } from '@vercel/speed-insights/react'
-import { Analytics } from '@vercel/analytics/next'
+import { Analytics } from '@vercel/analytics/react'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 import { AuthProvider } from '../components/AuthProvider'
